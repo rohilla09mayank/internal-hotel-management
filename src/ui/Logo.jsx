46e39313ebbed2c97df5b0b9ad307e99c95540c4
MayankRohilla17299 <52850405+MayankRohilla17299@users.noreply.github.com>
2025-06-06 23:10:45 +0530

    Supabase Connection + Layout

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -4,15 +4,23 @@ const StyledLogo = styled.div`
   text-align: center;
 `;
 
-const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
+// const Img = styled.img`
+//   height: 9.6rem;
+//   width: auto;
+// `;
+
+const LogoText = styled.p`
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 2rem;
+  color: var(--color-brand-500);
 `;
 
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      {/* <Img src="/logo-light.png" alt="Logo" /> */}
+      <LogoText>Admin Panel</LogoText>
     </StyledLogo>
   );
 }
